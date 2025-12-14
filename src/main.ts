c740a242a3a94d25c38a870c6a20a4885844daca
thughari <thughari3@gmail.com>
2025-12-14 22:32:01 +0530

    404

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get('redirect');
+
+if (redirect) {
+  history.replaceState(null, '', redirect);
+}
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
